b280acbc5066a31db68ec39d4eb1101b3078d962
BobbyHen <45887234+BobbyHen@users.noreply.github.com>
2019-06-14 09:47:37 -0400

    Adding components

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Styling Tittle component with JSX object
+const TitleStyle = {
+  color: "#000"
+}
+
+const Title = () => (
+    <div style={TitleStyle}>
+      <h2 className="titleText">Ron Swanson Quotes</h2>
+    </div>
+)
+
+export default Title
